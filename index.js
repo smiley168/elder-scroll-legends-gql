2634d628b2bb5b8b2cc827a53f9023a8eb5ff82b
Linda Lei <lindalei@Lindas-MacBook-Pro.local>
2020-05-02 01:06:17 -0700

    enable playground in production

--- index.js
+++ index.js
@@ -86,7 +86,9 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  introspection: true,
+  playground: true,
 });
 
 server.listen().then(({ url }) => {
